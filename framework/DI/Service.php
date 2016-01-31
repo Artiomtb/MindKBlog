a24c250afe6840239138f552d992ed563b3f257b
Artiomtb <artiomtb@gmail.com>
2016-01-31 17:27:28 +0200

    Service: implemented Service class
    Implemented logic of Dependency Injection. Added methods for set and multiple set dependencies, method for get class by service name. Also added method that returns all service names

--- framework/DI/Service.php
+++ framework/DI/Service.php
@@ -4,9 +4,45 @@ namespace Framework\DI;
 
 class Service
 {
+    private static $services = array();
 
+    /**
+     * Метод для получения зарегистрированного сервиса по имени
+     * @param $serviceName имя сервиса
+     * @return object|null экземпляр сервиса, или null, если не зарегистрировано сервиса с таким именем
+     */
     public static function get($serviceName)
     {
-        return "";
+        return array_key_exists($serviceName, self::$services) ? self::$services[$serviceName] : null;
+    }
+
+    /**
+     * Метод для регистрации сервиса по имени
+     * @param $serviceName имя сервиса
+     * @param $serviceClass класс сервиса
+     */
+    public static function set($serviceName, $serviceClass)
+    {
+        self::$services[$serviceName] = $serviceClass;
+    }
+
+    /**
+     * Метод для множественной регистрации сервисов по имени
+     * @param array $services ассоциативный массив в формате имя сервиса => класс сервиса (с namespace)
+     */
+    public static function setAll(array $services)
+    {
+        foreach ($services as $serviceName => $serviceClass) {
+            self::set($serviceName, $serviceClass);
+        }
+    }
+
+    /**
+     * Метод для получения всех имен зарегистрированных сервисов
+     * @return array из имен сервисов
+     */
+    public static function getAllServiceNames()
+    {
+        return array_keys(self::$services);
     }
 }
