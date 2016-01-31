3059fe6cf725daf787649444921b8db72eab2c0a
Artiomtb <artiomtb@gmail.com>
2016-01-31 21:45:27 +0200

    Request: added php docs and get($name) method

--- framework/Request/Request.php
+++ framework/Request/Request.php
@@ -2,6 +2,10 @@
 
 namespace Framework\Request;
 
+/**
+ * Class Request класс для работы с реквестом
+ * @package Framework\Request
+ */
 class Request
 {
 
@@ -10,6 +14,13 @@ class Request
     public $post;
     public $get;
 
+    /**
+     * Приватный конструктор для реквеста.
+     * @param $method_p string метод
+     * @param $uri_p string uri
+     * @param $post_p array параметры POST
+     * @param $get_p array параметры GET
+     */
     private function __construct($method_p, $uri_p, $post_p, $get_p)
     {
         $this->method = $method_p;
@@ -19,22 +30,49 @@ class Request
         return $this;
     }
 
+    /**
+     * Статическая функция для создания ревеста из глобальных серверных переменных
+     * @return Request экземпляр реквеста
+     */
     public static function create()
     {
         return new Request($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"], $_POST, $_GET);
+
     }
 
-
+    /**
+     * Проверяет, является ли метод данного реквеста = POST
+     * @return bool
+     */
     public function isPost()
     {
         return $this->method == "POST";
     }
 
+    /**
+     * Возвращает значение переменной POST запроса по ключу
+     * @param $name string ключ
+     * @return mixed значение переменной
+     */
     public function post($name)
     {
         return $this->post[$name];
     }
 
+    /**
+     * Возвращает значение переменной GET запроса по ключу
+     * @param $name string ключ
+     * @return mixed значение переменной
+     */
+    public function get($name)
+    {
+        return $this->get[$name];
+    }
+
+    /**
+     * Возвращает uri текущего реквеста
+     * @return string uri реквеста
+     */
     public function getUri()
     {
         return $this->uri;
