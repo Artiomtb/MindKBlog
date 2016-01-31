6f1708072f5c9f9edd55b4bcb6586364d9254646
Artiomtb <artiomtb@gmail.com>
2016-01-31 22:17:56 +0200

    Request: optimized

--- framework/Request/Request.php
+++ framework/Request/Request.php
@@ -14,20 +14,13 @@ class Request
     public $post;
     public $get;
 
+    //TODO добавить фильтрацию переменных
+
     /**
      * Приватный конструктор для реквеста.
-     * @param $method_p string метод
-     * @param $uri_p string uri
-     * @param $post_p array параметры POST
-     * @param $get_p array параметры GET
      */
-    private function __construct($method_p, $uri_p, $post_p, $get_p)
+    private function __construct()
     {
-        $this->method = $method_p;
-        $this->uri = $uri_p;
-        $this->post = $post_p;
-        $this->get = $get_p;
-        return $this;
     }
 
     /**
@@ -36,7 +29,7 @@ class Request
      */
     public static function create()
     {
-        return new Request($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"], $_POST, $_GET);
+        return new Request();
 
     }
 
@@ -46,7 +39,7 @@ class Request
      */
     public function isPost()
     {
-        return $this->method == "POST";
+        return $_SERVER["REQUEST_METHOD"] == "POST";
     }
 
     /**
@@ -56,7 +49,7 @@ class Request
      */
     public function post($name)
     {
-        return $this->post[$name];
+        return $_POST[$name];
     }
 
     /**
@@ -66,7 +59,7 @@ class Request
      */
     public function get($name)
     {
-        return $this->get[$name];
+        return $_GET[$name];
     }
 
     /**
@@ -75,6 +68,6 @@ class Request
      */
     public function getUri()
     {
-        return $this->uri;
+        return $_SERVER["REQUEST_URI"];
     }
 }
