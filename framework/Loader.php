b8639902d78f9477b1bd01a0554a61f6390ea4d0
Artiomtb <artiomtb@gmail.com>
2016-01-31 14:20:16 +0200

    Loader: added comments and phpdocs

--- framework/Loader.php
+++ framework/Loader.php
@@ -1,9 +1,11 @@
 <?php
 
+//инициализация лоадера-синглтона
 Loader::getInstance();
 
 class Loader
 {
+    //namespace и путь к классам framework
     const FRAMEWORK_NAMESPACE_NAME = "Framework\\";
     const FRAMEWORK_PATH = __DIR__ . "\\..\\framework";
 
@@ -12,7 +14,10 @@ class Loader
 
     private function __construct()
     {
+        //зарегистрируем классы фреймворка
         self::addNamespacePath(self::FRAMEWORK_NAMESPACE_NAME, self::FRAMEWORK_PATH);
+
+        //регистрируем функцию для поиска классов
         spl_autoload_register(function ($className) {
             foreach (self::$namespaces as $namespaceName => $namespacePath) {
                 $path = $namespacePath . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, str_replace($namespaceName, "", $className)) . ".php";
@@ -24,6 +29,10 @@ class Loader
         });
     }
 
+    /**
+     * Возвращает лоадер-синглтон
+     * @return Loader экземпляр лоадера
+     */
     public static function getInstance()
     {
         if (!isset(self::$instance)) {
@@ -36,11 +45,20 @@ class Loader
     {
     }
 
+    /**
+     * Сообщает автолоадеру, по какому пути искать классы с указанным namespace
+     * @param $namespaceName namespace класса, с "/" в конце
+     * @param $namespacePath полный путь к директории, где лежат классы
+     */
     public static function addNamespacePath($namespaceName, $namespacePath)
     {
         self::$namespaces[$namespaceName] = $namespacePath;
     }
 
+    /**
+     * Возвращет все ранее добавленные namespaces
+     * @return mixed ассоциативный массив namepspace => путь
+     */
     public static function getNamespaces()
     {
         return self::$namespaces;
