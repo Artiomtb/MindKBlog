d07ea7b258d3fe4fc2ede17eed5d63f7ed2380b4
Artiomtb <artiomtb@gmail.com>
2016-01-31 21:37:58 +0200

    UserInterface: added phpdocs

--- framework/Security/Model/UserInterface.php
+++ framework/Security/Model/UserInterface.php
@@ -4,7 +4,15 @@
 namespace Framework\Security\Model;
 
 
+/**
+ * Interface UserInterface интерфейс, описывающий пользователя. Необходимо имплментировать функцию getRole()
+ * @package Framework\Security\Model
+ */
 interface UserInterface
 {
+    /**
+     * Возвращает роль пользователя
+     * @return mixed роль пользователя
+     */
     public function getRole();
 }
