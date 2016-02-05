f7907442ce3a242211454ccab560042f964b63b0
Artem Siliuk <asiliuk@mindk.com>
2016-02-05 17:29:56 +0200

    Response: added php docs

--- framework/Response/ResponseRedirect.php
+++ framework/Response/ResponseRedirect.php
@@ -3,12 +3,17 @@
 
 namespace Framework\Response;
 
-
+/**
+ * Class ResponseRedirect класс респонса, отваечающий за перенаправление
+ * @package Framework\Response
+ */
 class ResponseRedirect extends Response
 {
 
     /**
      * ResponseRedirect constructor.
+     * @param string $route путь для перенаправления
+     * @param string $message сообщение, с которым будет выполнено перенаправление (добавляется в GET параметры с ключом redirectmessage)
      */
     public function __construct($route, $message = "")
     {
