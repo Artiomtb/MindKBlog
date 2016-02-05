a74ea1d663b4c8ed7c8a32ca4a71e0257ee3f2b3
Artem Siliuk <asiliuk@mindk.com>
2016-02-05 17:05:58 +0200

    Response: implemented JsonResponse class

--- framework/Response/JsonResponse.php
+++ framework/Response/JsonResponse.php
@@ -3,8 +3,19 @@
 
 namespace Framework\Response;
 
-
-class JsonResponse
+/**
+ * Class JsonResponse класс для отправки респонса типа json
+ * @package Framework\Response
+ */
+class JsonResponse extends Response
 {
 
+    /**
+     * JsonResponse constructor.
+     * @param array $array_json ассоциативный массив, который будет преобразован в json
+     */
+    public function __construct($array_json)
+    {
+        parent::__construct(json_encode($array_json), ResponseType::OK, "application/json");
+    }
 }
