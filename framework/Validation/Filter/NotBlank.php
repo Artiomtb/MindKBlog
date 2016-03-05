2d7eac5cc130aa920e013dc2fc0d17d3355835a6
Artiomtb <artiomtb@gmail.com>
2016-03-05 13:05:45 +0200

    Validation: implemented NotBlank filter

--- framework/Validation/Filter/NotBlank.php
+++ framework/Validation/Filter/NotBlank.php
@@ -3,8 +3,24 @@
 
 namespace Framework\Validation\Filter;
 
-
-class NotBlank
+/**
+ * Фильтр валидации для проверки является ли стока заполненной
+ * @package Framework\Validation\Filter
+ */
+class NotBlank extends AbstractValidationFilter
 {
-
+    /**
+     * Возврашает true, если переданное значение не является пустім
+     * @param string $value строка для проверки
+     * @return bool результат
+     */
+    function isValid($value)
+    {
+        if (strlen(trim($value)) > 0) {
+            return true;
+        } else {
+            $this->error = "Value should not be blank";
+            return false;
+        }
+    }
 }
