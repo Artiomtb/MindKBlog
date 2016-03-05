f3b55729cf3769e026d17f22c01e0598ce791172
Artiomtb <artiomtb@gmail.com>
2016-03-05 13:11:52 +0200

    Validation: fixed errors on doc

--- framework/Validation/Filter/NotBlank.php
+++ framework/Validation/Filter/NotBlank.php
@@ -4,13 +4,13 @@
 namespace Framework\Validation\Filter;
 
 /**
- * Фильтр валидации для проверки является ли стока заполненной
+ * Фильтр валидации для проверки, является ли стока заполненной
  * @package Framework\Validation\Filter
  */
 class NotBlank extends AbstractValidationFilter
 {
     /**
-     * Возврашает true, если переданное значение не является пустім
+     * Возврашает true, если переданное значение не является пустым
      * @param string $value строка для проверки
      * @return bool результат
      */
